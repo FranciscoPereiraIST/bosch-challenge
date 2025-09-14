3632e1dee313c1627611df749b5d440c9193153c
FranciscoPereiraIST <francisco.s.pereira@tecnico.ulisboa.pt>
2025-09-14 10:54:10 +0100

    Extracted EmissionsList

--- main_3.py
+++ main_3.py
@@ -2,8 +2,16 @@ import requests
 import pandas as pd
 import time
 
+def inspect_df(df: pd.DataFrame, name: str = "DataFrame", n: int = 5):
+    """Prints basic info about a DataFrame: its name, shape, and head rows."""
+    print(f"\nInspecting {name}")
+    print(f"Shape: {df.shape[0]} rows × {df.shape[1]} cols")
+    print(f"Preview (first {n} rows):")
+    print(df.head(n))
+    
 class FuelEconomyAPI:
     BASE_URL = "https://fueleconomy.gov/ws/rest/vehicle"
+    BASE_MPG_URL = "https://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle"
     HEADERS = {"Accept": "application/json"}
     ENDPOINTS = {
         "get_years": "/menu/year",
@@ -52,8 +60,22 @@ class FuelEconomyAPI:
             return r.json()
         except ValueError:
             print(f"Vehicle details for {vehicle_id} not in JSON format")
+            return None
+        
+    def get_MPG_summary(self, vehicle_id: str) -> dict:
+        endpoint = f"{self.BASE_MPG_URL}/{vehicle_id}"
+        r = requests.get(endpoint, headers=self.HEADERS)
+        
+        if r.status_code == 204:
+            print(f"No content returned for vehicle_id={vehicle_id}")
             return {}
-
+        
+        try:
+            return r.json()
+        except ValueError:
+            print(f"MPG Summary for {vehicle_id} not in JSON format")
+            return None
+        
 
 class Vehicle:
     def __init__(self, vehicle_id: str, year: int, make: str, model: str, api_client: FuelEconomyAPI):
@@ -69,14 +91,30 @@ class Vehicle:
 
     def get_fuel_info(self):
         details_json = self.api.get_vehicle_details(self.id)
-        self.emissionsList = details_json.pop("emissionsList", [])
+        self.emissionsList = details_json.pop("emissionsList", {})
+        self.emissions_flag_exist = False
+        self.emissions_df = None
+        if self.emissionsList:
+            self.emissions_flag_exist = True
+            # print(f"EmissionsList type is {type(self.emissionsList)}, {self.emissionsList}")
         self.fuel_raw = details_json
 
     def process_fuel_info(self):
         vehicle_dict = {"vehicle_id": self.id}
         vehicle_dict.update(self.fuel_raw)
         self.processed_df = pd.DataFrame([vehicle_dict])
-
+        
+    def process_emissions_list(self):
+        if self.emissions_flag_exist and len(self.emissionsList) > 0:
+            print(f"EmissionsList is NOT empty.", "\n") # ex: id = 31873
+            emissions_df = pd.DataFrame(self.emissionsList["emissionsInfo"])
+            # inspect_df(emissions_df)
+            self.emissions_df = emissions_df
+            
+    def get_MPG_summary_info(self):
+        mpg_summary = self.api.get_MPG_summary(self.id)
+        if len(mpg_summary) > 0:
+            print(mpg_summary)
 
 class Model:
     def __init__(self, name: str, make: str, year: int, api_client: FuelEconomyAPI):
@@ -97,11 +135,11 @@ class Model:
         return f"{self.year} - {self.make} - {self.name} with {len(self.vehicles)} vehicle_ids"
 
 class FuelEconomyETL:
-    def __init__(self, num_years=1, filename="FuelEconomy"):
+    def __init__(self, num_years=1):
         self.api = FuelEconomyAPI()
         self.vehicles = []
         self.num_years =  num_years
-        self.filename = filename
+        # self.filename = filename
 
     def extract(self):
         
@@ -116,7 +154,7 @@ class FuelEconomyETL:
         for y in years:
             makes = self.api.get_makes(y)
             limit_flag = False
-            limit_val = 5
+            limit_val = 3
             if len(makes) > limit_val:
                 max_l = limit_val
                 limit_flag = True
@@ -139,7 +177,7 @@ class FuelEconomyETL:
             mdl.fetch_vehicle_ids()
             vids_array.extend(mdl.get_vehicle_ids())
             
-        # vids_array.append(Vehicle(31873, 2025, "test", "test model", self.api))
+        vids_array.append(Vehicle(31873, 2025, "test", "test model", self.api))
 
         self.vehicles = vids_array
 
@@ -147,6 +185,7 @@ class FuelEconomyETL:
         
         print(f"Started Processing.....")
         df_array = []
+        df_emissions = []
         
         total_vehicles = len(self.vehicles) 
         
@@ -159,6 +198,10 @@ class FuelEconomyETL:
             vehicle.get_fuel_info()
             vehicle.process_fuel_info()
             df_array.append(vehicle.processed_df)
+            vehicle.process_emissions_list()
+            df_emissions.append(vehicle.emissions_df)
+            
+            vehicle.get_MPG_summary_info()
             
             # Calculate percentage completed
             percent_complete = ((idx+1) / total_vehicles) * 100
@@ -166,19 +209,24 @@ class FuelEconomyETL:
                 print(f"Processing: {int(percent_complete)}% complete ({idx+1}/{total_vehicles} vehicles)")
                 print(f"\t-Vehicle {idx+1} - {vehicle}")
                 next_print += milestone
+                
+            if idx >= 4:
+                break
             
         self.df_fuel = pd.concat(df_array)
+        self.df_emissions = pd.concat(df_emissions)
 
-    def write_to_csv(self):
+    def write_to_csv(self, df : pd.DataFrame, filename : str, df_name : str = "DataFrame"):
         current_time = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"raw_datasets/{self.filename}_{len(self.vehicles)}_vehicles_{current_time}.csv"
-        self.df_fuel.to_csv(filename, index=False)
-        print(f"Data written to file '{filename}' ({self.df_fuel.shape[0]} rows, {self.df_fuel.shape[1]} cols)")
+        filename = f"raw_datasets/{filename}_{len(self.vehicles)}_vehicles_{current_time}.csv"
+        df.to_csv(filename, index=False)
+        print(f"Dataframe '{df_name}' written to file '{filename}' ({df.shape[0]} rows, {df.shape[1]} cols)")
 
     def run_all(self):
         self.extract()
         self.process()
-        self.write_to_csv()
+        self.write_to_csv(df = self.df_fuel, filename = "FuelEconomy", df_name = "fuel_info")
+        self.write_to_csv(df = self.df_emissions, filename = "Emissions", df_name = "emissions")
 
 if __name__ == "__main__":
     
