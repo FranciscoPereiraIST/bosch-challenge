0a4d80afcd265b93e7a6bec6d40c21aac531e1bc
FranciscoPereiraIST <francisco.s.pereira@tecnico.ulisboa.pt>
2025-09-14 16:32:46 +0100

    Added a new version for creating concurrent requests

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from utils.extract_fuel_economy import FuelEconomyETL
-import time
+# from utils.extract_fuel_economy import FuelEconomyETL
 
-def main():
-    start_time = time.time()  # start timer
+from utils.fuel_economy_async import FuelEconomyETL
+from time import perf_counter
+import asyncio
+
+def main_2():
     
-    etl = FuelEconomyETL(num_years = 2)
-    # etl.run_all(filename="TEST")
-    etl.run_all()
+    time_before = perf_counter()
     
-    end_time = time.time()  # end timer
-    elapsed_time = end_time - start_time
-    print(f"\nTotal processing time for FuelEconomy source: {elapsed_time/60:.2f} minutes.")
+    etl = FuelEconomyETL(num_years=2, concurrency=20)
+    asyncio.run(etl.run_all())
+    
+    duration_in_secs = perf_counter() - time_before
+    print(f"Total time (asynchronous): {duration_in_secs:.3f} s -> {duration_in_secs/60:.1f} min")
+
+# def main():
+#     start_time = time.time()  # start timer
+    
+#     etl = FuelEconomyETL(num_years = 2)
+#     # etl.run_all(filename="TEST")
+#     etl.run_all()
+    
+#     end_time = time.time()  # end timer
+#     elapsed_time = end_time - start_time
+#     print(f"\nTotal processing time for FuelEconomy source: {elapsed_time/60:.2f} minutes.")
     
 if __name__ == "__main__":
-    main()
+    main_2()
